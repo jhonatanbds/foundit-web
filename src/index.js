27b633abeb7a66a2e7494f6f7d41c4f8bf171d5f
jhonatanbds <jhonatanbds@outlook.com>
2018-11-14 14:00:14 -0300

    Router DOM added, dashboard view, layout and route added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss";
+import "assets/demo/demo.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import indexRoutes from "routes/index.jsx";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} key={key} component={prop.component} />;
+      })}
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
