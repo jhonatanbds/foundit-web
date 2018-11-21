9be9270d4f5cfeb1edab34d9d79974bf3ecd0a57
Jhonatan Batista <jhonatanbds@outlook.com>
2018-11-21 16:48:06 -0300

    Updates

---
+++ precache-manifest.5b2ac964f66258f977c1d94cb7c29c9f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "087a9706bfcb8973b646",
+    "url": "/foundit-web/static/css/main.b1eb0d36.chunk.css"
+  },
+  {
+    "revision": "087a9706bfcb8973b646",
+    "url": "/foundit-web/static/js/main.087a9706.chunk.js"
+  },
+  {
+    "revision": "806b1489d08de9af4bda",
+    "url": "/foundit-web/static/css/1.db72e276.chunk.css"
+  },
+  {
+    "revision": "806b1489d08de9af4bda",
+    "url": "/foundit-web/static/js/1.806b1489.chunk.js"
+  },
+  {
+    "revision": "bff5a73d509304caa957",
+    "url": "/foundit-web/static/js/runtime~main.bff5a73d.js"
+  },
+  {
+    "revision": "3333f262eed9a0c8e19728fee529e1de",
+    "url": "/foundit-web/static/media/damir-bosnjak.3333f262.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/foundit-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/foundit-web/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/foundit-web/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/foundit-web/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/foundit-web/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "f41fc848c86f27db16e0475e2f527c35",
+    "url": "/foundit-web/index.html"
+  }
+];
