0e1d15b81c9bb9e5b8f3f023f6f563cd3cf68b4b
Jhonatan Batista <jhonatanbds@outlook.com>
2018-12-14 15:30:21 -0300

    item refactor

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,42 +6,35 @@ class Item extends Component {
   constructor(props) {
     super(props);
     this.delete = this.delete.bind(this);
-    this.toggle = this.toggle.bind(this);
     this.state = {
       popoverOpen: false
     };
-    console.log(this.props.obj);
   }
 
-  toggle() {
-    this.setState({
-      popoverOpen: !this.state.popoverOpen
-    });
-  }
-  contact() {
-
-  }
   delete() {
+    console.log("delete");
     axios
       .delete("http://localhost:3000/item/" + this.props.obj._id)
       .then(console.log("Deleted"))
       .catch(err => console.log(err));
+    window.location.reload();
   }
   
   render() {
     return (
       <ListGroupItem header="Titulo">
-      <div className="d-flex w-100 justify-content-between">
-        <h5 className="mb-1">{this.props.obj.title}</h5>
-        <small className="text-muted">{"Found at " + this.props.obj.foundDate}</small>
-      </div>
-      <span class="pull-right">
-      <a href="mailto:admin@email.com">
-        <button class="btn btn-xs btn-info" >Contact</button>
-      </a>
-      </span>
-      <p class="mb-1">{this.props.obj.description}</p>
-      <small class="text-muted">{this.props.obj.foundPlace}</small>         
+        <div className="d-flex w-100 justify-content-between">
+          <h5 className="mb-1">{this.props.obj.title}</h5>
+          <small className="text-muted">{"Found at " + this.props.obj.foundDate}</small>
+        </div>
+        <span className="pull-right">
+          {this.props.owner ? 
+            <Button className="btn btn-xs btn-danger" onClick={this.delete}>Delete</Button>
+            :<a href="mailto:admin@email.com"><button class="btn btn-xs btn-info" >Contact</button></a>
+          }      
+        </span>
+        <p className="mb-1">{this.props.obj.description}</p>
+        <small className="text-muted">{this.props.obj.foundPlace}</small>         
       </ListGroupItem>
     );
   }
